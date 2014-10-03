32ac8c63bc097fc808db14b410876d0863433e42
dvinograd <dvinogradov@edmunds.com>
2014-10-02 17:11:36 -0700

    Cleaning up search results and adding include-attrs parameter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 require 'edmunds-chef-rails-proxy/common'
 
+def filter2(hsh, *keys)
+  hsh.reject { |k, _| keys.include? k }
+end
+
 class SearchController < ApplicationController
   # GET /search
   def index
@@ -10,7 +14,19 @@ class SearchController < ApplicationController
   # GET /search/:id
   def show
     result = process_request(request)
-    render :json => JSON.pretty_generate(JSON.parse(result["body"])), :status => result["status"]
+    json_body = JSON.parse(result["body"])
+    if json_body["rows"]
+      json_body["rows"].each do |row|
+        ["automatic","default","normal","override"].each do |top_attr|
+          if request.params["include-attrs"]
+            row[top_attr].delete_if {|k,v| !request.params["include-attrs"].split(",").include? k}
+          else
+            row.delete_if {|k,v| k == top_attr}
+          end
+        end
+      end
+    end
+    render :json => JSON.pretty_generate(json_body), :status => result["status"]
   end
 
   # POST /search
