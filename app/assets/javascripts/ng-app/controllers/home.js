7886f8700d001ee4abb5d38c79ccab071d1fd744
Dmitriy Vinogradov <dvinogradov@edmunds.com>
2014-11-10 22:36:10 -0800

    Adding dashboard views

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,4 @@
-angular.module('AngularRails')
+angular.module('myApp')
     .controller('HomeCtrl', function ($scope) {
-        $scope.things = ['Angular', 'Rails 4.1', 'Working', 'Together!!'];
+        $scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
     });
