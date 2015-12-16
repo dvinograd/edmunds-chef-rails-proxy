b4e0605a1e13ef5a797a6dd751907b309a282d31
Dmitriy Vinogradov <dvinogradov@edmunds.com>
2015-12-16 00:13:44 -0800

    Updating config

--- config/initializers/proxy_config.rb
+++ config/initializers/proxy_config.rb
@@ -4,10 +4,16 @@ EdmundsChefRailsProxy::Application.config.user_keys_dir = "/etc/edmunds-chef-rai
 
 EdmundsChefRailsProxy::Application.config.chef_servers = [
     {
-        :url => "http://chef11-preprod.vip.edmunds.com:80",
-        :client_name => "proxyrw",
-        :client_key => "/etc/edmunds-chef-rails-proxy/keys/chef11-preprod_proxyrw.pem",
+        :url => "http://chef12-app-1.prod-admin11.vip.aws1/organizations/edmunds-qa",
+        :client_name => "svc-deliverysystems",
+        :client_key => "/etc/edmunds-chef-rails-proxy/keys/chef12-svc-deliverysystems.pem",
         :envs => ["dev.*", "qa.*", ".*sandbox"]
+    },
+    {
+        :url => "http://chef12-app-1.prod-admin11.vip.aws1/organizations/edmunds-prod",
+        :client_name => "svc-deliverysystems",
+        :client_key => "/etc/edmunds-chef-rails-proxy/keys/chef12-svc-deliverysystems.pem",
+        :envs => ["prod-eps11"]
     },
     {
         :url => "http://chef11-prod.vip.edmunds.com:80",
